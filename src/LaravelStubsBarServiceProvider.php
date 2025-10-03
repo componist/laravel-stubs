d65cd7acbfcd03937bf65da912137eeced283680
Reinhold-Jesse <reinhold.jesse@gmx.de>
2025-10-03 09:51:32 +0200

    init

---
+++ src/LaravelStubsBarServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Componist\LaravelStubs;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeveloperBarServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
